9ba1615a7872962f6875ce28257805f392d839a6
Junho Yeo <hanaro0704@gmail.com>
2021-03-30 01:12:52 +0900

    Create GlobalStyle for normalize & initialize default styles

---
+++ components/GlobalStyle.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components'
+import normalize from 'styled-normalize'
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  button {
+    border: 0;
+    cursor: pointer;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  input {
+    border: 0;
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
