5eb50db4fe2d7696f2f5519582ac1f58dbd33f96
Junho Yeo <hanaro0704@gmail.com>
2021-03-30 02:24:50 +0900

    Implement InvitationStatus

--- components/Home/InvitationStatus.tsx
+++ components/Home/InvitationStatus.tsx
@@ -2,7 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const InvitationStatus: React.FC = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <NumberOfPeopleJoined>7912</NumberOfPeopleJoined>
+      <TotalNumberOfInvitations>Until 10000</TotalNumberOfInvitations>
+    </Container>
+  );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 300px;
+`;
+
+const NumberOfPeopleJoined = styled.span`
+  font-weight: bold;
+  font-size: 4.2rem;
+`;
+
+const TotalNumberOfInvitations = styled.span`
+  font-weight: 300;
+  font-size: 2.5rem;
+`;
