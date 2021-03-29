711849a193525965da6431c7ca98b0ee83a72152
Junho Yeo <hanaro0704@gmail.com>
2021-03-30 05:20:05 +0900

    Show label overlay in used invite link

--- components/Dashboard/LinkRow.tsx
+++ components/Dashboard/LinkRow.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { toast } from 'react-toastify';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { Button } from '@/components/Common/Button';
 import { copyToClipboard } from '@/utils/clipboard';
@@ -8,9 +8,10 @@ import { copyToClipboard } from '@/utils/clipboard';
 type Props = {
   label: string;
   value: string;
+  isUsed: boolean;
 };
 
-export const LinkRow: React.FC<Props> = ({ label, value }) => {
+export const LinkRow: React.FC<Props> = ({ label, value, isUsed }) => {
   return (
     <Wrapper>
       <Label>{label}</Label>
@@ -18,12 +19,17 @@ export const LinkRow: React.FC<Props> = ({ label, value }) => {
         <Input value={value} />
         <CopyButton
           onClick={() => {
+            if (isUsed) {
+              return;
+            }
             copyToClipboard(value);
             toast('클립보드에 초대장 링크를 복사했습니다!');
           }}
+          isUsed={isUsed}
         >
           복사
         </CopyButton>
+        {isUsed && <UsedLabel>THIS INVITATION IS ALREADY USED</UsedLabel>}
       </Container>
     </Wrapper>
   );
@@ -42,6 +48,7 @@ const Label = styled.label`
 
 const Container = styled.div`
   display: flex;
+  position: relative;
 `;
 
 const Input = styled.input`
@@ -54,7 +61,11 @@ const Input = styled.input`
   border: 1px solid #dee2e6;
 `;
 
-const CopyButton = styled(Button)`
+type CopyButtonProps = {
+  isUsed: boolean;
+};
+
+const CopyButton = styled(Button)<CopyButtonProps>`
   width: 82px;
   height: 100%;
   padding: 16px 0;
@@ -64,7 +75,27 @@ const CopyButton = styled(Button)`
   background-color: #dee2e6;
   color: rgba(0, 0, 0, 0.65);
 
-  &:hover {
-    background-color: #ced4da;
-  }
+  ${({ isUsed }) =>
+    !isUsed
+      ? css`
+          &:hover {
+            background-color: #ced4da;
+          }
+        `
+      : css`
+          cursor: not-allowed !important;
+        `};
+`;
+
+const UsedLabel = styled.span`
+  position: absolute;
+  top: 0;
+  transform: rotate(8deg);
+  background: linear-gradient(to bottom right, #f4013e, #9f0322);
+  color: rgba(255, 255, 255, 0.85);
+  font-weight: 900;
+  font-size: 1.25rem;
+  padding: 12px;
+  border-radius: 3px;
+  cursor: not-allowed !important;
 `;
