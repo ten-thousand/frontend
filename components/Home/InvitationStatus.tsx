52c1141853a469f72d0e10f14f2ef653d85e465f
Junho Yeo <hanaro0704@gmail.com>
2021-03-30 02:16:11 +0900

    Restructure components used in Home

---
+++ components/Home/InvitationStatus.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const InvitationStatus: React.FC = () => {
+  return <Container></Container>;
+};
+
+const Container = styled.div``;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import styled from 'styled-components';
 
-import { PhoneAuthForm } from '@/components/PhoneAuth/PhoneAuthForm';
+import { InvitationStatus } from '@/components/Home/InvitationStatus';
+import { PhoneAuthForm } from '@/components/Home/PhoneAuth/PhoneAuthForm';
 
 const Home = () => {
   return (
     <Container>
+      <InvitationStatus />
       <PhoneAuthForm />
     </Container>
   );
@@ -15,4 +18,7 @@ export default Home;
 const Container = styled.div`
   min-height: 100vh;
   background-color: #f8f9fa;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
