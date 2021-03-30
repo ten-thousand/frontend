279757a65d956bbf6966253b2ff9975e8f752a5f
Junho Yeo <hanaro0704@gmail.com>
2021-03-31 04:48:10 +0900

    Implement useUserInformation hook

---
+++ hooks/useUserInformation.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+
+import { Client } from '@/utils/client';
+
+type UserReferral = {
+  inviteCode: string;
+  status: 'USED' | 'ISSUED';
+  usedBy: { phoneNumber: string; userName: string };
+};
+
+type UserInformation = {
+  userSerial: number;
+  userPhoneNumber: string;
+  userReferrals: UserReferral[];
+};
+
+export const useUserInformation: () => [
+  UserInformation | undefined,
+  string | undefined,
+] = () => {
+  const [userInformation, setUserInformation] = useState<
+    UserInformation | undefined
+  >();
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const {
+          data: { ok, data },
+        } = await Client.get('/user');
+        if (ok) {
+          const { userSerial, userPhoneNumber, userReferrals } = data;
+          setUserInformation({ userSerial, userPhoneNumber, userReferrals });
+          return;
+        }
+        setError('FailError');
+      } catch (error) {
+        console.log(error);
+        setError('RequestOrServerError');
+      }
+    };
+
+    getData();
+  }, []);
+
+  return [userInformation, error];
+};
