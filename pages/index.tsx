9e720b2cb9a2d3406e82da2b47e99b2c12bfe36b
Junho Yeo <hanaro0704@gmail.com>
2021-03-31 13:45:34 +0900

    Remove example helper links

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,22 +11,6 @@ const LandingPage = () => {
     <Container>
       <Clubhouse />
       <Splash />
-      {/* FIXME: remove example helper links */}
-      <ul
-        style={{
-          marginTop: 32,
-          listStyleType: 'disc',
-        }}
-      >
-        <li>
-          <Link href="/dashboard">{'/dashboard'}</Link>
-        </li>
-        <li>
-          <Link href="/referral/jHQdaiS/oAsdUUid">
-            {'/referral/jHQdaiS/oAsdUUid'}
-          </Link>
-        </li>
-      </ul>
     </Container>
   );
 };
