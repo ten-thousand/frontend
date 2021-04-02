ab2aff7ea15912b66bc38f23424fa923baadc739
Junho Yeo <hanaro0704@gmail.com>
2021-04-03 07:04:47 +0900

    Optional dependencies for useEffect in useBrowserEffect

--- hooks/useBrowserEffect.ts
+++ hooks/useBrowserEffect.ts
@@ -2,10 +2,13 @@ import { useEffect } from 'react';
 
 export const isBrowser = typeof window !== 'undefined';
 
-export const useBrowserEffect = (callback: () => void) => {
+export const useBrowserEffect = (
+  callback: () => void,
+  dependencies: any[] = [],
+) => {
   useEffect(() => {
     if (isBrowser) {
       callback();
     }
-  }, []);
+  }, [dependencies]);
 };
